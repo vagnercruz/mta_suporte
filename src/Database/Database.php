a1b452b67bb0e024bfc8e99445567255b2771136
vagnercruz <vagnerdacruz48@gmail.com>
2024-11-24 19:42:29 -0300

    Enviando o arquivo correto

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1,7 +1,21 @@
 <?php
-spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($file)) {
-        require $file;
+namespace Src\Database;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (self::$instance === null) {
+            try {
+                self::$instance = new PDO('mysql:host=localhost;dbname=mta_suporte;charset=utf8', 'root', '');
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+            }
+        }
+        return self::$instance;
     }
-});
+}
